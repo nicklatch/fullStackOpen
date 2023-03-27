73d02b9c19151707b966620faf5b040080d875f1
Nick <nick.latcham@proton.me>
2023-03-27 09:53:40 -0500

    add: components to handle returning respective data

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,26 +1,48 @@
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const parts = [
+    { part: "Fundamentals of React", exercises: 10 },
+    { part: "Using props to pass data", exercises: 7 },
+    { part: "State of a component", exercises: 14 },
+  ];
+  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises; // need to make this usable with abitrary amount of parts objects
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total total={total} />
     </div>
+  );
+};
+
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Content = (props) => {
+  console.log(props.parts);
+  return (
+    <div>
+      <p>
+        {props.parts[0].part} {props.parts[0].exercises}
+      </p>
+      <p>
+        {props.parts[1].part} {props.parts[1].exercises}
+      </p>
+      <p>
+        {props.parts[2].part} {props.parts[2].exercises}
+      </p>
+    </div>
+  );
+};
+
+const Total = (props) => {
+  console.log(props);
+  return (
+    <>
+      <p>Number of exercises {props.total}</p>
+    </>
   );
 };
 
