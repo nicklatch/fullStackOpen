945ddc8627db59ba7b6ce075646aa65532282c78
Nick Latcham <nick.latcham@proton.me>
2023-03-27 20:44:35 -0500

    refactor: const course const course is now a single object containing all needed data

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,42 +1,44 @@
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    { title: "Fundamentals of React", exercises: 10 },
-    { title: "Using props to pass data", exercises: 7 },
-    { title: "State of a component", exercises: 14 },
-  ];
-  const total = parts.reduce((curr, { exercises }) => curr + exercises, 0);
+  const course =
+  {
+    name: "Half Stack application development",
+    parts: [
+      { name: "Fundamentals of React", exercises: 10 },
+      { name: "Using props to pass data", exercises: 7 },
+      { name: "State of a component", exercises: 14 },
+    ],
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total total={total} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
 
-const Header = (props) => {
-  return <h1>{props.course}</h1>;
+const Header = (header) => {
+  console.log(header.course);
+  return <h1>{header.course}</h1>;
 };
 
-const Content = (props) => {
-  //needs refactor to add as many parts as there are parts indxes
+const Content = (content) => {
   return (
     <div>
-      <Part part={props.parts[0]} />
-      <Part part={props.parts[1]} />
-      <Part part={props.parts[2]} />
+      <Part part={content.parts[0]} />
+      <Part part={content.parts[1]} />
+      <Part part={content.parts[2]} />
     </div>
   );
 };
 
-const Part = (props) => {
-  console.log(props);
+const Part = (part) => {
+  console.log(part.part);
   return (
     <>
       <p>
-        {props.part.title} {props.part.exercises}
+        {part.part.name} {part.part.exercises}
       </p>
     </>
   );
@@ -45,7 +47,7 @@ const Part = (props) => {
 const Total = (props) => {
   return (
     <>
-      <p>Number of exercises {props.total}</p>
+      <p>Number of exercises {props.parts.reduce((curr, { exercises }) => curr + exercises, 0)}</p>
     </>
   );
 };
