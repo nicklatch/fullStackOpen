f66aee98663a2fbb15bc96c2fd40879618fef7a1
Nick <nick.latcham@proton.me>
2023-04-26 10:06:58 -0500

    removed

--- part2/countries/src/services/results.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-const baseUrl = "https://restcountries.com/v3.1/all";
-
-export const getInitialData = () => {
-  const request = axios.get(baseUrl);
-  return request.then((response) => response.data);
-};
