3de0f8f40af7aca13ec13e4d473be94c9c64908a
Nick Latcham <nick.latcham@proton.me>
2023-04-29 07:57:58 -0500

    chore(build): preping for production build launch

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cors = require("cors");
+const process = require("process");
 const app = express();
 const morgan = require("morgan");
 
@@ -80,7 +82,7 @@ app.post("/api/persons/", (request, response) => {
   response.json(persons);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
