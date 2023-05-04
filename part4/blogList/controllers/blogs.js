658c3c02591a1528e989a3f3f6aec670dc500e03
Nick <nick.latcham@proton.me>
2023-05-04 14:46:38 -0500

    refactor(backend): refactor backend into 4.2 spec
    create directories, files according to exercise 4.2 spec. moved logic into appropriate files. see diff and blame for context
    
    - Nick

---
+++ part4/blogList/controllers/blogs.js
@@ -0,0 +1,57 @@
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
+
+blogsRouter.get("/", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+blogsRouter.get("/:id", (request, response, next) => {
+  Blog.findById(request.params.id)
+    .then((blog) => {
+      blog ? response.json(blog) : response.status.apply(404).end();
+    })
+    .catch((error) => next(error));
+});
+
+blogsRouter.post("/", (request, response, next) => {
+  const body = request.body;
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  });
+
+  blog
+    .save()
+    .then((newBlog) => {
+      response.json(newBlog);
+    })
+    .catch((error) => next(error));
+});
+
+blogsRouter.put("/:id", (request, response, next) => {
+  const { title, author, url, likes } = request.body;
+
+  Blog.findByIdAndUpdate(
+    request.params.id,
+    { title, author, url, likes },
+    { new: true, runValidators: true, context: "query" }
+  )
+    .then((updatedBlog) => {
+      response.json(updatedBlog);
+    })
+    .catch((error) => next(error));
+});
+
+blogsRouter.delete("/:id", (request, response, next) => {
+  Blog.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+module.exports = blogsRouter;
