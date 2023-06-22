a3fa606b152dc4ddfd10a25dc00e358c10b5db46
Nick <nick.latcham@proton.me>
2023-06-22 11:31:57 -0500

    push to pull

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Routes, Route, useMatch } from 'react-router-dom';
+import { anecdotes as mockAnecdotes } from './mock/mockData';
 import Menu from './components/Menu';
 import About from './components/About';
 import AnecdoteList from './components/AnecdoteList';
@@ -9,22 +10,7 @@ import Footer from './components/Footer';
 import Notification from './components/Notification';
 
 const App = () => {
-  const [anecdotes, setAnecdotes] = useState([
-    {
-      content: 'If it hurts, do it more often',
-      author: 'Jez Humble',
-      info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
-      votes: 0,
-      id: 1,
-    },
-    {
-      content: 'Premature optimization is the root of all evil',
-      author: 'Donald Knuth',
-      info: 'http://wiki.c2.com/?PrematureOptimization',
-      votes: 0,
-      id: 2,
-    },
-  ]);
+  const [anecdotes, setAnecdotes] = useState(mockAnecdotes);
 
   const addNew = (anecdote) => {
     anecdote.id = Math.round(Math.random() * 10000);
