128d803c7a8905b79df7663d9ff419ee98750389
Nick Latcham <nick.latcham@proton.me>
2023-07-01 09:57:30 -0500

    .

--- part7/country-hooks/src/Country.jsx
+++ part7/country-hooks/src/Country.jsx
@@ -1,22 +1,22 @@
 const Country = ({ country }) => {
-  if (!country) {
-    return null;
-  }
-
-  if (!country.found) {
+  console.log(country.found);
+  if (!country || country.data === null) {
+    return <></>;
+  } else if (!country.found) {
     return <div>not found...</div>;
+  } else {
+    return (
+      <div>
+        <h3>{country.data.name.common} </h3>
+        <div>capital {country.data.capital} </div>
+        <div>population {country.data.population}</div>
+        <img
+          src={country.data.flags.png}
+          height="100"
+          alt={`${country.data.flags.alt}`}
+        />
+      </div>
+    );
   }
-  return (
-    <div>
-      <h3>{country.data.name} </h3>
-      <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div>
-      <img
-        src={country.data.flag}
-        height='100'
-        alt={`flag of ${country.data.name}`}
-      />
-    </div>
-  );
 };
 export default Country;
